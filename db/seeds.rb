5ba9e074e4882caa0d028535188d5eac685c5b5d
ryowuandjanet <ryowu0329@gmail.com>
2022-11-14 17:12:10 +0000

    Intro To Editing Comments With Stimulus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-User.create(email: 'ryowu0329@gmail.com',password: 'leo140814', password_confirmation: 'leo140814')
+User.create(email: 'dean@example.com', name: 'Dean', password: 'password', password_confirmation: 'password')
+User.create(email: 'John@doe.com', name: 'John', password: 'password', password_confirmation: 'password')
+User.create(email: 'ryowu0329@gmail.com', name: '吳小毛',password: 'leo140814', password_confirmation: 'leo140814')
+
 10.times do |x|
     Post.create(title: "Title #{x}",body: "Body #{x} Words go here Idk", user_id: User.first.id)
 end
